1e9e9534dfb65a5e7da7e2126e97f5ea86b097ea
Robert Pirritano <rpirritano@gmail.com>
2018-05-30 13:14:05 -0700

    changed to model-driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -9,11 +9,10 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 
-
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
