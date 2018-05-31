3929bbd5cc3a83ffb65f426682edcab51161c50c
Robert Pirritano <rpirritano@gmail.com>
2018-05-31 16:17:36 -0700

    refactored the string literals of service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,7 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 import { MediaItemService } from './media-item.service';
-
-const lookupLists = {
-  mediums: ['Movies', 'Series']
-};
+import { lookupListToken, lookupLists } from './providers';
 
 @NgModule({
   imports: [
@@ -29,7 +26,7 @@ const lookupLists = {
   ],
   providers: [
     MediaItemService,
-    { provide: 'lookupListToken', useValue: lookupLists }
+    { provide: lookupListToken, useValue: lookupLists }
   ],
   bootstrap: [
     AppComponent
